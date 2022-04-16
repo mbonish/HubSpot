1112d1e6a166370121e27bfc6827418cd3c9901b
Mariana Bonish <51426161+mbonish@users.noreply.github.com>
2022-04-16 12:00:15 -0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,58 @@
+function doForm(){
+    var xhr = new XMLHttpRequest();
+    var url = 'api.hsforms.com/submissions/v3/integration/submit/21798427/d17e9270-a1b2-4b5b-b64f-c4a07f018efc';
+    var firstName = document.getElementById('firstName').value;
+    var lastName = document.getElementById('lastName').value;
+    var email = document.getElementById('email').value;
+    
+    
+    var data = 
+           {"fields": [
+            {
+              "objectTypeId": "0-1",
+              "name": "email",
+              "value": email
+            },
+            {
+              "objectTypeId": "0-1",
+              "name": "firstname",
+              "value": firstName
+            },
+              {
+              "objectTypeId": "0-1",
+              "name": "lastname",
+              "value": lastName
+            }
+             
+          ],
+          "context": {
+            "hutk": ':hutk'}
+        };
+      var final_data = JSON.stringify(data)
+    
+    
+     
+      xhr.open('POST', url);
+      // Sets the value of the 'Content-Type' HTTP request headers to 'application/json'
+      xhr.setRequestHeader('Content-Type', 'application/json');
+  
+      xhr.onreadystatechange = function() {
+          if(xhr.readyState == 4 && xhr.status == 200) { 
+              alert(xhr.responseText); // Returns a 200 response if the submission is successful.
+          } else if (xhr.readyState == 4 && xhr.status == 400){ 
+              alert(xhr.responseText); // Returns a 400 error the submission is rejected.          
+          } else if (xhr.readyState == 4 && xhr.status == 403){ 
+              alert(xhr.responseText); // Returns a 403 error if the portal isn't allowed to post submissions.           
+          } else if (xhr.readyState == 4 && xhr.status == 404){ 
+              alert(xhr.responseText); //Returns a 404 error if the formGuid isn't found     
+          }
+         }
+  
+  
+      // Sends the request 
+      
+      xhr.send(final_data)
+   }
+  
+    
+   
